28b191fd8aa7be41e1d95b13c60ca61b50112929
hongsh429 <131283545+hongsh429@users.noreply.github.com>
2023-09-07 23:59:27 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 648 ms, Memory: 58548 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer firstRow = new StringTokenizer(br.readLine());
+
+        int numberCount = Integer.parseInt(firstRow.nextToken());
+        int rowCount = Integer.parseInt(firstRow.nextToken());
+
+        String[] stringFormatNumbers = br.readLine().split(" ");
+        StringBuilder builder = new StringBuilder();
+
+        int[] sumArray = new int[stringFormatNumbers.length];
+
+        for (int i = 0; i < stringFormatNumbers.length; i++) {
+            if (i == 0) {
+                sumArray[i] = Integer.parseInt(stringFormatNumbers[i]);
+            } else {
+
+                sumArray[i] = sumArray[i - 1] + Integer.parseInt(stringFormatNumbers[i]);
+            }
+        }
+
+
+        for (int i = 0; i < rowCount; i++) {
+            String[] row = br.readLine().split(" ");
+            int output = 0;
+            int start = Integer.parseInt(row[0]) - 1;
+            int end = Integer.parseInt(row[1]) - 1;
+            if (start == 0) {
+                output = sumArray[end];
+            } else {
+                output = sumArray[end] - sumArray[start-1];
+            }
+
+            builder.append(output).append("\n");
+        }
+
+        System.out.println(builder.toString());
+
+    }
+}
